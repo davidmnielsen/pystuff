2dd5ce79139844f99b230ec0f14c4e983d87cb75
David Nielsen <david.buzios@gmail.com>
2019-07-25 17:07:43 +0200

    its a real package now

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pystuff',
+      version='0.1',
+      description='Easy copy-and-paste python stuff to ease your life',
+      url='https://github.com/davidmnielsen/pystuff',
+      author='David Nielsen',
+      author_email='davidnielsen@id.uff.br',
+      license='MIT',
+      packages=['pystuff'],
+      zip_safe=False)
